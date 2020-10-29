3a2e52587d2c8070eeaa5610f1d803e605108469
Brandyn Reindel <BRANDYN.REINDEL@GMAIL.COM>
2020-10-28 20:29:53 -0400

    Cleaning Up Code and Documentation

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LevelSelect_2()
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    public void LevelSelect_3()
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+    public void LevelSelect_4()
+        {
+            SceneManager.LoadScene("Level 4");
+        }
+    public void LevelSelect_5()
+        {
+            SceneManager.LoadScene("Level 5");
+        }
+    public void LevelSelect_6()
+        {
+            SceneManager.LoadScene("Level 6");
+        }
+    public void LevelSelect_7()
+        {
+            SceneManager.LoadScene("Level 7");
+        }
+    public void LevelSelect_8()
+        {
+            SceneManager.LoadScene("Level 8");
+        }
+    public void LevelSelect_9()
+        {
+            SceneManager.LoadScene("Level 9");
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/Oscillator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2;
+     [Range(0,1)][SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved.
+
+    Vector3 startingPos;
+
+    // Use this for initialization
+	void Start ()
+    {
+        startingPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (period <= Mathf.Epsilon) //protect against period being zero
+        {
+            return;
+        }
+
+        float cycles = Time.time / period; //grows continually from zero
+        const float tau = Mathf.PI * 2f; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
